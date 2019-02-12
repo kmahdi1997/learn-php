87b435be4ba5b3acea7a2edeef226194c771edc8
Mahdi Karimi <kmahdi1997@gmail.com>
2019-02-12 14:25:45 +0330

    design logout

--- login/dashboard.php
+++ login/dashboard.php
@@ -10,5 +10,19 @@ if (!$_SESSION['login'])
 
 ?>
 
-<a href="logout.php">Logout</a>
-<p>Hi</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/vendors/bootstrap/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-3">
+        <a class="btn btn-danger btn-block" href="logout.php">Logout</a>
+        <h1 class="text-center mt-3">Hi</h1>
+    </div>
+</body>
+</html>
